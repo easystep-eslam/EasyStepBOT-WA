cbd87827d281b1206933df1aadf0cddb9f30fcaf
easystep-eslam <easystep5566@gmail.com>
2026-01-20 22:42:16 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settings = {
+  packname: 'EasyStep-BOT',
+  prefix: '!',
+  author: 'LRM',
+  botName: "EasyStep-BOT",
+  botOwner: 'Eslam',
+  ownerNumber: '201065537938',
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20,
+  storeWriteInterval: 10000,
+  description: "WhatsApp bot for managing groups and automating commands.",
+  version: "3.0.7",
+
+  updateZipUrl: "https://github.com/easystep-eslam/EasyStepBOT-WA",
+  github: "https://github.com/easystep-eslam/EasyStepBOT-WA",
+};
+
+module.exports = settings;
